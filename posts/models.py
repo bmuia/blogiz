4c2e5ca01fe1f614c5031be2b1af339ce2678315
belammuia <belammuia0@gmail.com>
2025-02-14 15:46:22 +0300

    changes

--- posts/models.py
+++ posts/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+  
     class Meta:
         unique_together = ('post', 'user')
 
